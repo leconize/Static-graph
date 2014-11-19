7784c006e756056117a942ead393aa2551076b1e
Suphawit Getmark <s7070117@kmitl.ac.th>
2014-11-19 19:38:15 +0700

    Change code to OOP

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 '''Project Graph creater'''
-from tkinter import *
-window = Tk()
-label = Label(window, text = "Graph creater")
-bar = Button(window, text = "Bar Graph", fg = "Blue")#Button for create bar graph
-circle = Button(window, text = "Circle Graph", fg = "Purple")#Button for create circle graph
-label.pack()
-bar.pack()
-circle.pack()
+from Tkinter import *
+
+class Test(Frame):
+    
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")
+         
+        self.parent = parent
+        
+        self.initUI()#call UI
+
+    def initUI(self): # User Interface 
+      
+        self.parent.title("Simple")
+        self.pack(fill=BOTH, expand=1)
+        label = Label(self, text = "Graph creater")
+        bar = Button(self, text = "Bar Graph", fg = "Blue")
+        circle = Button(self, text = "Circle Graph", fg = "Purple")
+        label.pack()
+        bar.pack()
+        circle.pack()
+        
+def main():
+    
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Test(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+    
