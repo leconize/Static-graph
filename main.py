7a7c970f45503f1a707f1560791a7cac9d8a9a1e
Suphawit Getmark <s7070117@kmitl.ac.th>
2014-11-20 21:26:46 +0700

    Testing how to get value by Entry

--- main.py
+++ main.py
@@ -10,31 +10,30 @@ class Test(Frame):
         
         self.initUI()#call UI
 
+
     def initUI(self): # User Interface 
       
         self.parent.title("Simple")
         self.pack(fill=BOTH, expand=1)
+        
         label = Label(self, text = "Graph Creater")
         label.grid(row = 0, column = 1)
         Label(self, text = "Data1").grid(row = 1)
-        Label(self, text = "Data2").grid(row = 2)
-        Label(self, text = "Data3").grid(row = 3)
-        Label(self, text = "Data4").grid(row = 4)
-        Label(self, text = "Data5").grid(row = 5)
-        data1 = Entry(self)
-        data2 = Entry(self)
-        data3 = Entry(self)
-        data4 = Entry(self)
-        data5 = Entry(self)
-        data1.grid(row = 1, column = 1)
-        data2.grid(row = 2, column = 1)
-        data3.grid(row = 3, column = 1)
-        data4.grid(row = 4, column = 1)
-        data5.grid(row = 5, column = 1)
+        
+        self.data1 = Entry(self)
+        self.data1.grid(row = 1, column = 1)
+        
         bar = Button(self, text = "Bar Graph", fg = "Blue")#Button for create bar graph
         circle = Button(self, text = "Circle Graph", fg = "Purple")#Button for create circle graph
         bar.grid(row = 6, column = 1)
         circle.grid(row = 6, column = 2)
+        
+        test = Button(self, text = "Testing", command=self.on_button)
+        test.grid(row = 7, column =1)
+
+        
+    def on_button(self):
+        print self.data1.get()
 
 
 def main():
