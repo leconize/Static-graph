a11fc3f61af193e86fb2da3fd9aa0f01da1047b2
Suphawit Getmark <s7070117@kmitl.ac.th>
2014-12-02 14:30:15 +0700

    Now can drawing circle graph, make listbox for user

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 '''Project Graph creater'''
 from Tkinter import *
 
-class Test(Frame):
+class App(Frame):
     
     count = 0
-    data = {}
+    data = []
     color = ["Tomato", "Chartreuse", "Darkturquoise", "Deeppink", "Gold"]
     
     def __init__(self, parent):
@@ -19,17 +19,27 @@ class Test(Frame):
         get value and value's name save them in to dic for calculate function
         '''
         self.count += 1
-        self.data[self.data_name.get()] = self.value.get()
+        name, value = self.data_name.get(), self.value.get()
+        self.data.append((name, value))
+        self.show_value.insert(END, name,value)
         print self.data
 
-    def create_window(self):
+    def drawing_circle(self):
         '''
         prototype function made for create graph
         '''
+        all_value = 0
+        for name, value in self.data:
+            all_value += float(value)    
         self.t = Toplevel(self)
         graph = Canvas(self.t, width = 600,height = 600, borderwidth=5, background='white')
-        graph.create_arc((10,10,152,152), outline = "#FAF402", fill = "#FAF402"\
-                         , start = 100, extent = 100)
+        start = 0
+        for i in xrange(self.count):
+            extent = float(self.data[i][1])*360/all_value
+            print start, extent
+            graph.create_arc((10,10,152,152), outline = self.color[i], fill = self.color[i]\
+                         , start = start, extent = extent)
+            start += extent
         graph.pack()
 
     def initUI(self): # User Interface
@@ -52,19 +62,21 @@ class Test(Frame):
         self.value.grid(row = 1, column = 3)
         
         bar = Button(self, text = "Bar Graph",bg = "Darkolivegreen", fg = "Aliceblue")
-        circle = Button(self, text = "Circle Graph", bg ="Darkolivegreen", fg = "Aquamarine",  command = self.create_window)
+        circle = Button(self, text = "Circle Graph", bg ="Darkolivegreen", fg = "Aquamarine",  command = self.drawing_circle)
         bar.grid(row = 6, column = 1)
         circle.grid(row = 6, column = 2)
         
-        test = Button(self, text = "Get_data", bg = "Navy", fg = "Deepskyblue", command = self.on_button)
-        test.grid(row = 7, column =1)
+        get_value = Button(self, text = "Get_data", bg = "Navy", fg = "Deepskyblue", command = self.on_button)
+        get_value.grid(row = 7, column =1)
 
+        self.show_value = Listbox()#make listbox for user can see what they input
+        self.show_value.pack()
+    
 
 def main():
-    
     root = Tk()
     root.geometry("400x200+300+300")
-    app = Test(root)
+    app = App(root)
     root.mainloop()
 
 if __name__ == '__main__':
