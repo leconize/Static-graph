7000c6c2f46395d3208cc8faeafa5e97af1d602d
Suphawit Getmark <s7070117@kmitl.ac.th>
2014-12-13 18:45:19 +0700

    Revert "fail placing"
    This reverts commit 53da9b9720d155fbe76b3961cfb3fa025aa5e4ae.

--- main.py
+++ main.py
@@ -51,32 +51,30 @@ class App(Frame):
         self.pack(fill=BOTH, expand=1)
         
         label = Label(self, text = "Graph Creater")
-        label.pack()
+        label.grid(row = 0, column = 1)
 
-        Label(self, text = "Name").pack(side = LEFT)
-        self.data_name = Entry(self)
-        self.data_name.pack(side = LEFT)
+        Label(self, text = "Name").grid(row = 1)
+        Label(self, text = "Value").grid(row = 1,column = 2)
         
-        Label(self, text = "Value").pack(side = LEFT)
+        self.data_name = Entry(self)
+        self.data_name.grid(row = 1, column = 1)
+
         self.value = Entry(self)
-        self.value.pack()
+        self.value.grid(row = 1, column = 3)
         
         bar = Button(self, text = "Bar Graph",bg = "Darkolivegreen", fg = "Aliceblue", command = lambda x = 1 : Graph(self, self.data, x))
         circle = Button(self, text = "Circle Graph", bg ="Darkolivegreen", fg = "Aquamarine",  command = lambda x = 0 : Graph(self, self.data, x))
-        container = Frame(self.parent)
-        container.pack(fill='both', expand=True)
-
-        #bar.grid(row = 0, column = 0)
-        circle.pack()
+        bar.grid(row = 6, column = 1)
+        circle.grid(row = 6, column = 2)
         
-        #get_value = Button(self, text = "Get_data", bg = "Navy", fg = "Deepskyblue", command = self.on_button)
-        #get_value.pack()
+        get_value = Button(self, text = "Get_data", bg = "Navy", fg = "Deepskyblue", command = self.on_button)
+        get_value.grid(row = 7, column =1)
 
-        #self.table = ttk.Treeview(self, columns=['Name', 'Value'], show = "headings")
-        #vsb = Scrollbar(orient='vertical', command=self.table.yview)
-        #hsb = Scrollbar(orient='horizontal', command=self.table.xview)
-        #self.table.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
-        #self.table.pack(fill = "both")
+        self.table = ttk.Treeview(self, columns=['Name', 'Value'], show = "headings")
+        vsb = Scrollbar(orient='vertical', command=self.table.yview)
+        hsb = Scrollbar(orient='horizontal', command=self.table.xview)
+        self.table.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+        self.table.grid(row = 8, column = 1)
 
         #vsb.grid(column=2,row=0,sticky ='ns')
         #hsb.grid(column=1,row=9,sticky='ew')
