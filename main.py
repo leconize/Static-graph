1938c5df9cb6131b44a33f13fa6c27489e4811bf
kijtana-ch <s7070006@kmitl.ac.th>
2014-12-14 14:06:58 +0700

    Separate the code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 '''Project Graph creater'''
 from Tkinter import *
+from PIL import Image,ImageTk
 import tkMessageBox
+import ttk
 
 class App(Frame):
     
-    count = 0
-    data = []
-    color = ["Tomato", "Chartreuse", "Darkturquoise", "Deeppink", "Gold", "Maroon","DarkBlue", "DarkKhaki", "SandyBrown", "LightSalmon", "IndianRed"]
+    data = [['a',10],['b',20]]
     
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -28,70 +28,13 @@ class App(Frame):
         except:
             tkMessageBox.showerror('Error', 'the value can not be word')
             return
-        self.count += 1
         self.data.append((name, int(value)))
         print self.data
+#-----------------------------------------------------------------------------------------------------------------------
 
-    def drawing_circle(self):
-        '''
-        prototype function made for create graph
-        '''
-        all_value = 0
-        for name, value in self.data:
-            all_value += float(value)    
-        self.t = Toplevel(self)
-        graph = Canvas(self.t, width = 600,height = 600, borderwidth=5, background='white')
-        start = 0
-        for i in xrange(self.count):
-            extent = float(self.data[i][1])*360/all_value
-            print start, extent
-            graph.create_arc((150,150,450,450), outline = self.color[i], fill = self.color[i]\
-                         , start = start, extent = extent)
-            start += extent
-        graph.pack()
-        Button(self.t, text = "Exit", command = self.t.destroy).pack()
-        
-
-    def draw_bar(self):
-        '''
-        Create bar graph from data
-        '''
-        self.bar = Toplevel(self)
-        graph = Canvas(self.bar, width = 600, height= 600, borderwidth = 5, background = 'white')
-        bar_width = 500/(self.count*2+1)
-        x_1 = bar_width
-        if max([float(self.data[i][1]) for i in xrange(self.count)]) > 500:
-            scale = 500/max([float(self.data[i][1]) for i in xrange(self.count)])
-        else:
-            scale = 1
-        for x in xrange(self.count):
-            graph.create_rectangle(x_1, 550-float(self.data[x][1])*scale, x_1+bar_width, 550, fill = self.color[x])
-            x_1 = x_1+bar_width+bar_width/2
-        graph.pack()
-        self.cal()
-
-    def cal(self):
-        '''
-        calcurate  max min average and medium
-        '''
-        lis = [self.data[i][1] for i in xrange(self.count)]
-        maximum = max(lis)
-        minimum = min(lis)
-        average = sum(lis)/len(lis)
-        lis.sort()
-        if len(lis)%2 == 0:
-            medium = (lis[len(lis)/2]+lis[len(lis)/2-1])/2
-        else:
-            medium = lis[len(lis)/2]
-        ans = Listbox(self)
-        ans.insert(END, maximum)
-        ans.insert(END, minimum)
-        ans.insert(END, average)
-        ans.insert(END, medium)
-        ans.pack()
     def initUI(self): # User Interface
         '''
-        User interface function
+        Main window of the program insert name and value to create the graph
         '''
         self.parent.title("Graph-Creater")
         self.pack(fill=BOTH, expand=1)
@@ -101,29 +44,146 @@ class App(Frame):
 
         Label(self, text = "Name").grid(row = 1)
         Label(self, text = "Value").grid(row = 1,column = 2)
-
-        self.data_name = StringVar()
+        
         self.data_name = Entry(self)
         self.data_name.grid(row = 1, column = 1)
 
-        self.value = DoubleVar()
         self.value = Entry(self)
         self.value.grid(row = 1, column = 3)
         
-        bar = Button(self, text = "Bar Graph",bg = "Darkolivegreen", fg = "Aliceblue", command = self.draw_bar)
-        circle = Button(self, text = "Circle Graph", bg ="Darkolivegreen", fg = "Aquamarine",  command = self.drawing_circle)
+        bar = Button(self, text = "Bar Graph",bg = "Darkolivegreen", fg = "Aliceblue", command = lambda x = 1 : Graph(self, self.data, x))
+        circle = Button(self, text = "Circle Graph", bg ="Darkolivegreen", fg = "Aquamarine",  command = lambda x = 0 : Graph(self, self.data, x))
         bar.grid(row = 6, column = 1)
         circle.grid(row = 6, column = 2)
         
         get_value = Button(self, text = "Get_data", bg = "Navy", fg = "Deepskyblue", command = self.on_button)
         get_value.grid(row = 7, column =1)
 
+        self.table = ttk.Treeview(self, columns=['Name', 'Value'], show = "headings")
+        vsb = Scrollbar(orient='vertical', command=self.table.yview)
+        hsb = Scrollbar(orient='horizontal', command=self.table.xview)
+        self.table.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+        self.table.grid(row = 8, column = 1)
         
+        #vsb.grid(column=2,row=0,sticky ='ns')
+        #hsb.grid(column=1,row=9,sticky='ew')
+#---------------------------------------------------------------------------------------------------------------------------------------
+class Graph:
+    '''
+    This Class will draw a bargraph and circlegraph from the data
+    '''
+    color = ["Tomato", "Chartreuse", "Darkturquoise", "Deeppink", "Gold", "Maroon","DarkBlue", "DarkKhaki", "SandyBrown", "LightSalmon", "IndianRed"]
     
+    def __init__(self, tk, data, mode):
+        self.graph = Toplevel(tk)
+        self.data = data
+        self.count = len(data)
+        self.value = [i[1] for i in self.data]
+        if mode == 1:
+            self.draw_bar()
+        else:
+            self.draw_circle()
+        self.namebox()
+        self.cal()
+    def draw_circle(self):
+        '''
+        Draw the Circle graph from the data
+        '''
+        all_value = 0
+        for name, value in self.data:
+            all_value += float(value)    
+        graph = Canvas(self.graph, width = 600,height = 600, borderwidth=5, background='white')
+        start = 0
+        for i in xrange(self.count):
+            extent = float(self.data[i][1])*360/all_value
+            print start, extent
+            graph.create_arc((150,150,450,450), outline = self.color[i], fill = self.color[i]\
+                         , start = start, extent = extent)
+            start += extent
+        graph.pack(side= LEFT)
+        #Button(self.t, text = "Exit", command = self.t.destroy).pack()
 
+    def draw_bar(self):
+        '''
+        Draw the Bar graph from the data
+        '''
+        graph = Canvas(self.graph, width = 600, height= 600, borderwidth = 5, background = 'white')
+        size = 575
+        bar_width = 500/(self.count*3+1)
+        x_1 = bar_width+50
+        temp = 0
+        level = 10
+        while max(self.value) > temp:
+            for i in [1, 2 ,5]:
+                temp = level*i
+                if temp >= max(self.value):
+                    break
+            level *= 10
+        scale = 500.0/temp
+        for x in xrange(self.count):
+            graph.create_rectangle(x_1,575-float(self.data[x][1])*scale, x_1+bar_width*2, 575, fill = self.color[x])
+            x_1 = x_1+bar_width*3
+        #y-axis
+        graph.create_line(50,25,50,575)
+        #y-axis arrow
+        graph.create_line(50,25,60,35)
+        graph.create_line(50,25,40,35)
+        for i in xrange(5):
+            mark = (temp-(temp/5)*i)*scale
+            graph.create_text(25,575-mark, text=str(temp-(temp/5)*i))
+            graph.create_line(45,575-mark,55,575-mark)
+        #x-axis
+        graph.create_line(50,575,550,575)
+        #x-axis arrow
+        graph.create_line(550,575,540,585)
+        graph.create_line(550,575,540,565)
+        graph.pack(side = LEFT)
+        #self.cal(self.graph)
+#----------------------------------------------------------------------------------------------------------------------- 
+    def namebox(self):
+        '''
+        Show the name and value of the Data
+        '''
+        box = Canvas(self.graph, width = 200, height = 300, background = 'white')
+        name_list = [i[0] for i in self.data]
+        temp = [10,30]
+        for i in xrange(2):
+            box.create_rectangle(10,temp[0],30,temp[1], fill = self.color[i])
+            box.create_text(40,(temp[0]+temp[1])/2, text = name_list[i])
+            temp = map(lambda x : x+30, temp)  
+        frame = ttk.Frame(self.graph, relief=RAISED, borderwidth=10)
+        frame.pack(side = LEFT, fill = Y, expand = 1)
+        box.pack(side =TOP, anchor = NE)
+    
+    def cal(self):
+        '''
+        calcurate  the Statistic value that include max min average and medium
+        '''
+        lis = [self.data[i][1] for i in xrange(self.count)]
+        maximum, minimum, average = max(lis), min(lis), sum(lis)/len(lis)
+        lis.sort()
+        if len(lis)%2 == 0:
+            medium = (lis[len(lis)/2]+lis[len(lis)/2-1])/2
+        else:
+            medium = lis[len(lis)/2]
+        ans = Listbox(self.graph)
+        ans.insert(END, "Maximum" +" "+" = " + str(maximum))
+        ans.insert(END, "Minimum" +" "+" = " + str(minimum))
+        ans.insert(END, "Average" + " "+ " = " + str(average))
+        ans.insert(END, "Medium" + " "+ " = " + str(medium))
+        Label(self.graph, text = 'Statistic value').pack(side=TOP, anchor = N)
+        ans.pack(side = TOP, fill = 'both' ,expand= 1)
+#-----------------------------------------------------------------------------------------------------------------        
 def main():
+    '''
+    Call the main window
+    '''
     root = Tk()
-    root.geometry("400x200+300+300")
+    img = Image.open("icon.gif")
+    tkpi = ImageTk.PhotoImage(img)
+    label_image = Label(root, image=tkpi)
+    label_image.pack()
+    root.resizable(width=FALSE, height=FALSE)
     app = App(root)
     root.mainloop()
 
